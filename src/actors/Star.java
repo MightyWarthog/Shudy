eb41edcf3e4d83b3a4b885340736103408c1782e
Owen Walpole <owengithub@gmail.com>
2017-05-03 02:23:49 -0500

    Uniformly(ish) distribute stars
    >mfw not using the Halton Sequence

--- src/actors/Star.java
+++ src/actors/Star.java
@@ -1,52 +1,74 @@
 package actors;
 
 import mayflower.Actor;
+import mayflower.MayflowerImage;
 
 public class Star extends Actor
 {
-	boolean b;
-	//boolean enlarge;
+	private boolean b;
+	private boolean bb;
 	
-	public Star()
-	{
-		this("assets/img/actors/star.gif");
-	}
+	private int x;
+	private int y;
 	
-	public Star(String img)
+	private MayflowerImage img;
+	
+	private final int TRANSPARENCY = (int) ( Math.random() * 90 ) + 10;
+	private final int DELTA = (int) ( Math.random() * 4 + 1 );
+	
+	public Star(int x, int y)
 	{
-		setImage(img);
 		setRotation( (int) ( Math.random() * 361 ) );
 		
-		final int transparency = (int) ( Math.random() *90 )+10;
-		getImage().setTransparency(transparency);
+		if ( TRANSPARENCY % 4 == 0 )
+			img = new MayflowerImage("assets/img/actors/star32x32.gif");
+		else if ( TRANSPARENCY % 4 == 1 )
+			img = new MayflowerImage("assets/img/actors/star16x16.gif");
+		else if ( TRANSPARENCY % 4 == 3 )
+			img = new MayflowerImage("assets/img/actors/star16x16.gif");
+		else
+			img = new MayflowerImage("assets/img/actors/star16x16.gif");
 		
-		b = ( transparency  % 2 == 0 )? true : false;
-		//enlarge = b;
+		img.setTransparency(TRANSPARENCY);
+		setImage(img);
+		
+		b = ( TRANSPARENCY  % 2 == 0 )? true : false;
+		bb = b;
+		
+		this.x = x;
+		this.y = y;
 	}
 
 	@Override
 	public void act()
 	{
 		if (b)
-			setRotation( getRotation()+1 );
+			setRotation( getRotation() + (int) ( Math.random() * 4 ) + 1 );
 		else
-			setRotation( getRotation()-1 );
+			setRotation( getRotation() - (int) ( Math.random() * 4 ) + 1 );
 		
-		/*
-		MayflowerImage scaled = getImage();
-		if (enlarge)
-		{
-			scaled.scale(scaled.getWidth()+1, scaled.getHeight()+1);
-			setLocation( getX()-1, getY()-1 );
-		}
+		if ( img.getTransparency() >= 100 || img.getTransparency() <= 0 )
+			bb = !bb;
+		
+		if ( bb )
+			img.setTransparency( img.getTransparency() + DELTA );
 		else
-		{
-			scaled.scale(scaled.getWidth()-1, scaled.getHeight()-1);
-			setLocation( getX()+1, getY()+1 );
-		}
-		setImage(scaled);
-		*/
-		
-		//enlarge = ( (int) ( Math.random() * 32 ) == 31 )? !enlarge : enlarge;
+			img.setTransparency( img.getTransparency() - DELTA );
+	}
+	
+	@Override
+	public int getX()
+	{ return x; }
+	
+	@Override
+	public int getY()
+	{ return y; }
+	
+	@Override
+	public void setLocation(double x, double y)
+	{
+		super.setLocation(x, y);
+		this.x = (int) ( x + 0.5 );
+		this.y = (int) ( y + 0.5 );
 	}
 }
