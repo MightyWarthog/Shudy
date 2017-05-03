314bd68c86bde839e4341851c53a64c0d2875799
Owen Walpole <owengithub@gmail.com>
2017-05-03 02:21:10 -0500

    Move star generating code to ShudyWorld.java

--- src/worlds/MainMenu.java
+++ src/worlds/MainMenu.java
@@ -3,7 +3,6 @@ package worlds;
 import org.newdawn.slick.SlickException;
 
 import actors.QuitButton;
-import actors.Star;
 import actors.WorldChangeButton;
 import engine.Utils;
 
@@ -12,18 +11,18 @@ import mayflower.Timer;
 
 public class MainMenu extends ShudyWorld
 {
-	private Timer t;
+	private final Timer t;
 	
 	public MainMenu()
 	{
 		if ( WIDESCREEN )
 			try
-			{ setBackground( Utils.getScaledImage("assets/img/worlds/Level1_169.png", getWidth(), getHeight()) ); }
+			{ setBackground( Utils.getScaledImage("assets/img/worlds/starfield_169.png", getWidth(), getHeight()) ); }
 			catch(SlickException e)
 			{ e.printStackTrace(); }
 		else
 			try
-			{ setBackground( Utils.getScaledImage("assets/img/worlds/Level1_43.png", getWidth(), getHeight()) ); }
+			{ setBackground( Utils.getScaledImage("assets/img/worlds/starfield_43.png", getWidth(), getHeight()) ); }
 			catch(SlickException e)
 			{ e.printStackTrace(); }
 		
@@ -38,8 +37,85 @@ public class MainMenu extends ShudyWorld
 		
 		t = new Timer();
 		
+		//final ArrayList<Integer> randomX = new ArrayList<Integer>(WIDTH/128) {{ for ( int i = 0; i < WIDTH; i += 8 ) add(i); }};
+		//final ArrayList<Integer> randomY = new ArrayList<Integer>(HEIGHT/128) {{ for ( int i = 0; i < HEIGHT; i += 8 ) add(i); }};
+		
+		//int i = 1024;
+		//while ( randomX.size() > 1 && randomY.size() > 1 && i > 0 )
+			//addObject( new Star(), randomX.remove( (int) ( Math.random() * randomX.size() ) ), randomY.remove( (int) ( Math.random() * randomY.size() ) ) );
+		
+		//Collections.shuffle(randomX);
+		//Collections.shuffle(randomY);
+		
+		//while ( randomX.size() > 0 && randomY.size() > 0 )
+		//	addObject( new Star(), randomX.remove(0), randomY.remove(0) );
+		
+		
+		//Halton h = new Halton(2, 3);
+		//for ( int i = 0; i < 1024; i++ )
+		//	addObject(  new Star(), (int) ( Utils.getHaltonNumber(2, i+2) + 0.5 ), (int) ( Utils.getHaltonNumber(2, i+2) + 0.5 ) );
+		
+		//Random r = new Random( System.nanoTime() );
+		//for ( int i = 0; i < 512; i++ )
+		//	addObject( new Star(), r.nextInt(WIDTH), r.nextInt(HEIGHT) );
+		
+		//randIdx = N * i / X + random.nextInt(N / X) + 1;
+		
+		
+		/*
+		System.out.println(1);
+		
+		final int[] randomInts = new Random().ints(2048, 0, HEIGHT).distinct().toArray();
+		
+		System.out.println(2);
+		
+		ArrayList<Integer> random = new ArrayList<Integer>(2048) {{ for ( int i : randomInts ) add(i); }};
+		
+		System.out.println(3);
+		
+		int i = 1024;
+		while ( random.size() > 1 && i > 0 )
+		{
+			int r1 = (int) ( Math.random() * random.size() -1 );
+			int r2 = (int) ( Math.random() * random.size() -1 );
+			
+			int rr1 = random.get(r1);
+			int rr2 = random.get(r2);
+			
+			System.out.println(5);
+			
+			random.remove(r1);
+			random.remove(r2);
+			
+			System.out.println(6);
+			
+			addObject(new Star(), rr1, rr2);
+			i--;
+		}
+		*/
+		
+		//ArrayList<Integer> random = new ArrayList<Integer>();
+		//for (int r = 0; r < WIDTH*HEIGHT; r++)
+		//	random.add(r);
+		
+		//final Star[] stars = new Star[1024];
+		
+		/*
 		for ( int i = 0; i < 1024; i++ )
-			addObject( new Star(), Mayflower.getRandomNumber(getWidth()), Mayflower.getRandomNumber(getHeight()) );
+		{
+			int r1 = (int) ( Math.random() * random.size() );
+			int r2 = (int) ( Math.random() * random.size() );
+			
+			int rr1 = random.get(r1);
+			int rr2 = random.get(r2);
+			
+			random.remove(r1);
+			random.remove(r2);
+			
+			addObject( new Star(), rr1, rr2);
+		}
+		*/
+			//stars[i] = new Star();
 	}
 	
 	@Override
@@ -47,10 +123,11 @@ public class MainMenu extends ShudyWorld
 	{
 		super.act();
 		
+		//TODO: use better sound API
 		final String[] sounds = {"assets/snd/menu_1.wav"};
 		int index = Mayflower.getRandomNumber(sounds.length);
 		
-		if ( SOUND && !Utils.isPlaying && t.isDone() )
+		if ( SOUND && t.isDone() && !Utils.isPlaying )
 		{
 				t.set(133000);
 				Mayflower.playSound(sounds[index]);
--- src/worlds/ShudyWorld.java
+++ src/worlds/ShudyWorld.java
@@ -7,6 +7,7 @@ import actors.ProjLaser;
 import actors.ShudyActor;
 import actors.Star;
 import engine.Utils;
+
 import mayflower.World;
 
 import org.ini4j.Wini;
@@ -14,22 +15,39 @@ import org.ini4j.Wini;
 public abstract class ShudyWorld extends World
 {
 	private static final Wini ini = Utils.getConfig("settings.ini");
-	
-	//public static final int WIDTH = Integer.parseInt( ini.get("Display", "width") );
-	//public static final int HEIGHT = Integer.parseInt( ini.get("Display", "height") );
+
+	public static final int WIDTH = Integer.parseInt( ini.get("Display", "width") );
+	public static final int HEIGHT = Integer.parseInt( ini.get("Display", "height") );
 	public static final int RATE = Integer.parseInt( ini.get("Display", "rate") );
-	
+
 	public static final boolean VSYNC = Boolean.parseBoolean( ini.get("Display", "vsync") );
 	public static final boolean FULLSCREEN = Boolean.parseBoolean( ini.get("Display", "fullscreen") );
 	public static final boolean FPS = Boolean.parseBoolean( ini.get("Display", "showFPS") );
 	public static final boolean WIDESCREEN = Boolean.parseBoolean( ini.get("Display", "widescreen") );
-	
 	public static final boolean SOUND = Boolean.parseBoolean( ini.get("Sound", "enabled") );
-	
+
 	public ShudyWorld()
-	{ setPaintOrder(Star.class, ProjLaser.class, ShudyActor.class, Label.class); }
-	
+	{ this(true); }
+
+	public ShudyWorld(boolean drawStars)
+	{
+		setPaintOrder(Star.class, ProjLaser.class, ShudyActor.class, Label.class);
+
+		if (drawStars)
+			generateStars();
+	}
+
 	@Override
 	public void act()
 	{ Display.sync(RATE); }
+
+	private void generateStars()
+	{
+		for ( int w = 0; w < WIDTH; w += 64 )
+			for ( int h = 0; h < HEIGHT; h += 64 )
+			{
+				Star s = new Star(w + (int) ( Math.random() * 32 ) - 16, h + (int) ( Math.random() * 32 ) - 16);
+				addObject( s, s.getX(), s.getY() );
+			}
+	}
 }
