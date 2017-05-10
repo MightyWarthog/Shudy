5535024d6fb0eef903ecb8091f8b226ea2ed5910
Owen Walpole <owengithub@gmail.com>
2017-05-09 22:25:27 -0500

    Change enemy image

--- src/worlds/Level1.java
+++ src/worlds/Level1.java
@@ -29,14 +29,14 @@ public class Level1 extends ShudyWorld
 		karel = new Player();
 		addObject(karel, 400, 300);
 		
-		Grunt hector = new Grunt(100, 10, "assets/img/actors/player.gif");
-		addObject(hector, 600, 200);
+		//Grunt hector = new Grunt(100, 10, "assets/img/actors/player.gif");
+		//addObject(hector, 600, 200);
 		
 		//Grunt god = new Grunt(Integer.MAX_VALUE, 5, "assets/img/monster.png");
 		//addObject(god, 800, 400);
 		
-		for (int i = 0; i < 4; i++)
-			addObject( new Grunt(50, 10, "assets/img/cookie.png"), Mayflower.getRandomNumber(getWidth()), Mayflower.getRandomNumber(getHeight()) );
+		//for (int i = 0; i < 4; i++)
+		//	addObject( new Grunt(50, 10, "assets/img/cookie.png"), Mayflower.getRandomNumber(getWidth()), Mayflower.getRandomNumber(getHeight()) );
 		
 		scoreCard = new textLabel( new MayflowerImage("Score: " + points, 36, new Color(0, 136, 0) ) );
 		addObject( scoreCard, getWidth() / 2 + 300, getHeight()-50 );
@@ -55,7 +55,7 @@ public class Level1 extends ShudyWorld
 		
 		if ( spawner.isDone() )
 		{
-			addObject( new Grunt(50, 10, "assets/img/actors/player.gif"), Mayflower.getRandomNumber(WIDTH), Mayflower.getRandomNumber(HEIGHT) );
+			addObject( new Grunt(50, 10, "assets/img/actors/enemy.gif"), Mayflower.getRandomNumber(WIDTH), Mayflower.getRandomNumber(HEIGHT) );
 			spawner.set(3000);
 		}
 	}
