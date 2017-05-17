4d2acc52ac14e7a8e0b77d48a2afe84643885e12
Owen Walpole <owengithub@gmail.com>
2017-05-16 22:15:27 -0500

    Use mayflower.Label

--- src/worlds/Level1.java
+++ src/worlds/Level1.java
@@ -3,23 +3,18 @@ package worlds;
 import actors.Grunt;
 import actors.Player;
 import items.HealthPack;
-import mayflower.Actor;
 import mayflower.Color;
+import mayflower.Label;
 import mayflower.Mayflower;
-import mayflower.MayflowerImage;
 import mayflower.Timer;
 
 public class Level1 extends ShudyWorld
 {
 	protected int points;
 	
-	private int lastPoints;
-	private int lastHealth;
-	private int lastAmmo;
-	
-	private textLabel scoreCard;
-	private textLabel healthCard;
-	private textLabel ammoCard;
+	private Label scoreCard;
+	private Label healthCard;
+	private Label ammoCard;
 	
 	private Player karel;
 	
@@ -34,27 +29,14 @@ public class Level1 extends ShudyWorld
 		karel = new Player();
 		addObject(karel, 400, 300);
 		
-		//Grunt hector = new Grunt(100, 10, "assets/img/actors/player.gif");
-		//addObject(hector, 600, 200);
-		
-		//Grunt god = new Grunt(Integer.MAX_VALUE, 5, "assets/img/monster.png");
-		//addObject(god, 800, 400);
-		
-		//for (int i = 0; i < 4; i++)
-		//	addObject( new Grunt(50, 10, "assets/img/cookie.png"), Mayflower.getRandomNumber(getWidth()), Mayflower.getRandomNumber(getHeight()) );
-		
-		scoreCard = new textLabel( new MayflowerImage("Score: " + points, 36, new Color(0, 136, 0) ) );
+		scoreCard = new Label( "Score: " + points, 36, new Color(0, 136, 0) );
 		addObject( scoreCard, getWidth() / 2 + 300, getHeight()-50 );
 		
-		healthCard = new textLabel( new MayflowerImage("Health: " + karel.getHealth(), 36, new Color(0, 136, 0) ) );
+		healthCard = new Label( "Health: " + karel.getHealth(), 36, new Color(0, 136, 0) );
 		addObject( healthCard, getWidth() / 2 - 300, getHeight()-50 );
 		
-		ammoCard = new textLabel( new MayflowerImage("Ammo: ∞", 36, new Color(0, 136, 0) ) );
+		ammoCard = new Label( "Ammo: \u221e", 36, new Color(0, 136, 0) );
 		addObject( ammoCard, 200, getHeight()-50 );
-		
-		lastHealth = karel.getHealth();
-		lastPoints = 0;
-		lastAmmo = karel.getAmmo();
 		
 		addObject( new HealthPack(), 800, 400 );
 	}
@@ -64,28 +46,9 @@ public class Level1 extends ShudyWorld
 	{
 		super.act();
 		
-		if ( lastPoints != points )
-		{
-			scoreCard.setImage( new MayflowerImage("Score: " + points, 36, new Color(0, 136, 0) ) );
-			lastPoints = points;
-		}
-		
-		//this method still has an FPS hit when in runs
-		//fine for scoreCard but maybe something else for healthCard?
-		if ( lastHealth != karel.getHealth() )
-		{
-			healthCard.setImage( new MayflowerImage("Health: " + karel.getHealth(), 36, new Color(0, 136, 0) ) );
-			lastHealth = karel.getHealth();
-		}
-		
-		if ( lastAmmo != karel.getAmmo() )
-		{
-			if ( karel.getAmmo() == -1 )
-				ammoCard.setImage( new MayflowerImage("Ammo: ∞", 36, new Color(0, 136, 0) ) );
-			else
-				ammoCard.setImage( new MayflowerImage("Ammo: " + karel.getAmmo(), 36, new Color(0, 136, 0) ) );
-			lastAmmo = karel.getAmmo();
-		}
+		healthCard.setText( String.valueOf( karel.getHealth() ) );
+		scoreCard.setText( String.valueOf( points ) );
+		ammoCard.setText( String.valueOf( karel.getAmmo() ) );
 		
 		if ( spawner.isDone() )
 		{
@@ -95,16 +58,5 @@ public class Level1 extends ShudyWorld
 	}
 	
 	public void addPoints(int p)
-	{ points += p; }
-	
-	//Temporary class for testing scoreCard and healthCard
-	private class textLabel extends Actor
-	{
-		public textLabel(MayflowerImage img)
-		{ setImage(img); }
-		
-		@Override
-		public void act()
-		{ }
-	}
+	{ points += p; }	
 }
